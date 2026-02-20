8f866a699bca5c8cf93100377c2295fbe74fd980
Ashishd167 <dalalashish167@gmail.com>
2026-02-20 12:33:42 +0530

    pipeline changes commit

--- codebuild/stop.sh
+++ codebuild/stop.sh
@@ -1,4 +1,19 @@
-cd /home/ubuntu/ae-manage
-docker-compose down || true
-cd ../
-rm -rf ae-manage
+#!/bin/bash
+set -e
+
+echo "Checking for any running Docker containers..."
+containers=$(docker ps -aq)
+
+if [ -n "$containers" ]; then
+  echo "Stopping and removing Docker containers..."
+  docker stop $containers || true
+  docker rm $containers || true
+else
+  echo "No Docker containers found."
+fi
+
+echo "Cleaning up application directory..."
+cd /home/ubuntu || true
+rm -rf ae-manage || true
+
+echo "Stop and cleanup completed."
