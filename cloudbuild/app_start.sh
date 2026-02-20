164080e3195545d0d07c61ff9686c4c86861942a
Ashishd167 <dalalashish167@gmail.com>
2026-02-20 14:51:32 +0530

    commit-02

--- cloudbuild/app_start.sh
+++ cloudbuild/app_start.sh
@@ -1,5 +1,5 @@
 cd /home/ubuntu/ae-manage
-gsutil cp gs://tj-testing-bucket/config.manage-ae.env config.env
+#gsutil cp gs://tj-testing-bucket/config.manage-ae.env config.env
 
 sudo docker build -t hello .
 sudo docker run hello
