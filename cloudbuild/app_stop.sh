628863a999ae1bc30934d0c86892a3c83502f08f
Ashishd167 <dalalashish167@gmail.com>
2026-02-20 17:01:49 +0530

    last

--- cloudbuild/app_stop.sh
+++ cloudbuild/app_stop.sh
@@ -3,7 +3,11 @@ set -e
 
 cd /home/ubuntu/ae-manage || exit 1
 
-docker compose down || true
+echo "Stopping containers..."
+sudo docker compose down --remove-orphans || true
+
+echo "Removing stopped containers..."
+sudo docker rm -f $(docker ps -aq) 2>/dev/null || true
 
 cd /home/ubuntu
-
+echo "Cleanup done."
