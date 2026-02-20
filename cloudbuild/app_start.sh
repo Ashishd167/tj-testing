5b4db9d1998b520cc0f0b67f242448949422ea6f
Ashishd167 <dalalashish167@gmail.com>
2026-02-20 17:16:04 +0530

    abc1

--- cloudbuild/app_start.sh
+++ cloudbuild/app_start.sh
@@ -4,7 +4,7 @@ set -e
 cd /home/ubuntu/ae-manage || exit 1
 
 echo "Downloading env file from GCS..."
-gsutil cp gs://tj-testing-bucket/config.manage-ae.env config.env
+gsutil cp gs://tj-testing-bucket/config.manage-ae.env.txt config.env
 
 echo "Starting application..."
 sudo chmod 777 /var/run/docker.sock
