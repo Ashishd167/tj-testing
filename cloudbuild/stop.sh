aa917910ee62fe6aa9932144dd7959b14cb86fb3
Ashishd167 <dalalashish167@gmail.com>
2026-02-20 15:40:14 +0530

    commit-05

--- cloudbuild/stop.sh
+++ cloudbuild/stop.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 set -e
 
-echo "Checking for any running Docker containers..."
-containers=$(docker ps -aq)
-
-if [ -n "$containers" ]; then
-  echo "Stopping and removing Docker containers..."
-  docker stop $containers || true
-  docker rm $containers || true
-else
-  echo "No Docker containers found."
+if [ "$(docker ps -aq)" ]; then
+  docker rm -f $(docker ps -aq)
 fi
 
-
+rm -rf /home/ubuntu/ae-manage || true
